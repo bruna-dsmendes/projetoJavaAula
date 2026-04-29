28feb5a0e563fae3160dfcdc1add109cca8e95d4
Bruna Mendes <brunadsantos.mendes@gmail.com>
2026-04-29 20:52:03 -0300

    Introdução a metodos e metodos externos

---
+++ src/metodoExterno/ExemploMetodoExterno.java
@@ -0,0 +1,11 @@
+package metodoExterno;
+
+public class ExemploMetodoExterno {
+
+	// Criando o método s/ retorno e s/ parametro
+		public static void info() {
+			System.out.println("Hello World");
+
+	}
+
+}
