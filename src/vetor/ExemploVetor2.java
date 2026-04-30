4438b2caecbb3e4e4b3d0575e408c4485414a27e
Bruna Mendes <brunadsantos.mendes@gmail.com>
2026-04-29 21:43:31 -0300

    Aula de Vetores e Collections

---
+++ src/vetor/ExemploVetor2.java
@@ -0,0 +1,32 @@
+package vetor;
+
+import java.util.Scanner;
+
+public class ExemploVetor2 {
+
+	public static void main(String[] args) {
+		
+		// Criando um vetor vazio com 3 posições
+				int vetorNumeros[] = new int[3];
+				
+				Scanner leia = new Scanner(System.in);
+				
+				// vetorNumero[0]
+				
+				// Popular/Adicionar itens ao vetor
+				for(int indice = 0; indice < 3; indice++) {
+					System.out.println("Digite um número: ");
+					vetorNumeros[indice] = leia.nextInt();
+					// numero = leia.nextInt();
+				}
+				
+				// Mostrar itens do vetor
+				for(int indice = 0; indice < 3; indice++) {
+					System.out.println((indice + 1) + "º elemento: " + vetorNumeros[indice]);
+				}
+				
+				System.out.println("Tamanho do Vetor: " + vetorNumeros.length);
+
+	}
+
+}
